12d5e63883811b125f8e4a8a50879ccf30c4e16b
sasukesurya <144013792+sasukesurya@users.noreply.github.com>
2023-10-28 01:54:05 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
+
+const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
+  return (
+    <div className="hero" id="home">
+      <div className="blur hero-blur"></div>
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <motion.div
+            initial={{ left: mobile ? "140px" : "215px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
+          <span>The Best Fitness Club near you</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>
+              In here we will guide you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>
+              <NumberCounter end={10} start={1} delay="2" preFix="+" />
+            </span>
+            <span> Expert coaches </span>
+          </div>
+          <div>
+            <span>
+              <NumberCounter end={178} start={80} delay="3" preFix="+" />
+            </span>
+            <span> members </span>
+          </div>
+          <div>
+            <span>
+              <NumberCounter end={30} start={11} delay="2" preFix="+" />
+            </span>
+            <span> fitness programs </span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn"> Join Now </button>
+
+        <motion.div
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={transition}
+          className="heart-rate"
+        >
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          src={hero_image_back}
+          alt=""
+          className="hero-image-back"
+        />
+
+        {/* calories */}
+        <motion.div
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+          className="calories"
+        >
+          <img src={Calories} alt="" />
+          <span> Calories Burned </span>
+          <span> 220 kcal </span>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,27 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: "DARSH DEVAIAH",
+    status: "ENTREPRENEUR",
+  },
+  {
+    image: image2,
+    review:
+      "Our gym is the ultimate fitness haven, where we sculpt bodies and empower minds, one workout at a time.",
+    name: "VIMAL",
+    status: "COACH",
+  },
+  {
+    image: image3,
+    review:
+      "This gym feels like my second home; the welcoming atmosphere and dedicated staff make every workout a joy",
+    name: "BRIJESH",
+    status: "CUSTOMER",
+  },
+];
